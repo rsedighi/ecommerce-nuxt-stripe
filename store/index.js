4ac7dced1e078f9c2a5ff927c05ed65e92595fe5
bhavana-netlify <42786844+bhavana-netlify@users.noreply.github.com>
2020-04-22 17:11:15 -0400

    Update index.js

--- store/index.js
+++ store/index.js
@@ -45,7 +45,7 @@ export const actions = {
     try {
       await axios
         .post(
-          "https://ecommerce-netlify.netlify.com/.netlify/functions/index",
+          "https://ecommerce-netlify.netlify.app/.netlify/functions/index",
           {
             stripeEmail: payload.stripeEmail,
             stripeAmt: Math.floor(getters.cartTotal * 100), //it expects the price in cents, as an integer
